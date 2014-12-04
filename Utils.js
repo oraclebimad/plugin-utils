4adddb07de1af49e35d1e2d6be4670323b226451
Sergio Medina <sergio.medina@oracle.com>
2014-12-04 17:15:44 -0600

    Added formatting method

--- Utils.js
+++ Utils.js
@@ -1,6 +1,23 @@
 (function (main) {
   'use strict';
   /* jshint unused:true, jquery:true, curly:false, browser:true */
+  var formats = {
+    raw: function (value) { return value; },
+    currency: function (value, opts) {
+      opts = Utils.isObject(opts) ? opts : {};
+      if (!opts.symbol)
+        opts.symbol = '$';
+      return otps.symbol + ' ' + formats.thousands(value, opts);
+    },
+    thousands: function (value, opts) {
+      var format = ',';
+      opts = Utils.isObject(opts) ? opts : {};
+      if (opts.decimals)
+        format += '2.';
+      format += 'f';
+      return d3.format(format)(value);
+    }
+  };
   var Utils = {
     proxy: function (fn, thisArg) {
       return function () {
@@ -26,6 +43,11 @@
     },
     isObject: function (obj) {
       return jQuery.isPlainObject(obj);
+    },
+    format: function (number, format, opts) {
+      if (!(format in formats))
+        format = 'raw';
+      return formats[format](number, opts);
     },
     isEmptyObject: jQuery.isEmptyObject,
     isArray: jQuery.isArray,
